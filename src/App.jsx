13c7a75d268cb9ec54e7cffa238d8f3c56563b3a
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-01-11 21:11:14 +0530

    hero section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="text-3xl text-pink">welcome to the world of GTA nad GSAP</div>
+    </main>
+  )
+}
+
+export default App
